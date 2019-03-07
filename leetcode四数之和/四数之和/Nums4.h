0742d47a282633b4cc1f6d067979fd064e6ecc48
honkerY <1497259719@qq.com>
2019-03-07 17:59:42 +0800

    ss

---
+++ leetcode四数之和/四数之和/Nums4.h
@@ -0,0 +1,27 @@
+/*给定数组 nums = [1, 0, -1, 0, -2, 2]，和 target = 0。
+[-2,-1,0,1,2]
+满足要求的四元组集合为：[
+  [-1,  0, 0, 1],
+  [-2, -1, 1, 2],
+  [-2,  0, 0, 2]
+  ]*/
+
+/*例如, 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+
+满足要求的三元组集合为：
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	vector<vector<int>> fourSum(vector<int>& nums, int target) {
+		 //[-2,-1,0,1,2]四数转三数转两数;
+
+	}
+};
